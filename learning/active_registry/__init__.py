aa7bfe68c528c395c2a176f56197289decd7ee99
Rattanachai Pahtaradiloak <boc.rpfunds@gmail.com>
2026-07-24 14:07:16 +0700

    Export PR157 trusted event contracts

--- learning/active_registry/__init__.py
+++ learning/active_registry/__init__.py
@@ -1,5 +1,12 @@
 """Canonical Active Knowledge Registry public API."""
-from .models import ActiveKnowledgeEntry, STATUSES
+from .models import ActiveKnowledgeEntry, EVENT_TYPES, STATUSES
 from .registry import ActiveKnowledgeRegistry
 from .storage import ActiveRegistryStorage
-__all__ = ["ActiveKnowledgeEntry", "ActiveKnowledgeRegistry", "ActiveRegistryStorage", "STATUSES"]
+
+__all__ = [
+    "ActiveKnowledgeEntry",
+    "ActiveKnowledgeRegistry",
+    "ActiveRegistryStorage",
+    "EVENT_TYPES",
+    "STATUSES",
+]
